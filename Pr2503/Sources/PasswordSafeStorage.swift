8008ca5e1eed99234d2de4ff80cbea296f895240
ivanovdale <dania-spb@mail.ru>
2024-03-17 13:28:43 +0300

    create password safe storage

---
+++ Pr2503/Sources/PasswordSafeStorage.swift
@@ -0,0 +1,53 @@
+//
+//  PasswordSafeStorage.swift
+//  Pr2503
+//
+//  Created by Daniil (work) on 15.03.2024.
+//
+
+import Foundation
+
+final class PasswordSafeStorage {
+    private let queue: DispatchQueue
+
+    private var passwordParts: [Int: String] = [:]
+
+    var passwordReader: String {
+        var result = ""
+        print("Start reading...")
+        queue.sync {
+            print("Reading...")
+            let count = passwordParts.count
+            for index in 0..<count {
+                let value = passwordParts[index]
+                if let value {
+                    result.append(value)
+                }
+            }
+        }
+        return result
+    }
+
+    var unsafePasswordReader: String? {
+        var result = ""
+        let count = passwordParts.count
+        for index in 0..<count {
+            let value = passwordParts[index]
+            if let value {
+                result.append(value)
+            }
+        }
+        return result
+    }
+
+    init(queue: DispatchQueue) {
+        self.queue = queue
+    }
+
+    func writePassword(newValue: String, at index: Int) {
+        queue.async(flags: .barrier) {
+            self.passwordParts[index] = newValue
+            print("Writing...")
+        }
+    }
+}
